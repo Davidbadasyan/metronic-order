3de7d3111cde630240b9a51b704ca43979dd3d6f
Davit Badasyan <davit.badasyan@volo.global>
2023-04-10 20:01:06 +0400

    error interceptor

---
+++ src/core/services/interceptors/errorInterceptor.ts
@@ -0,0 +1,9 @@
+import { ElMessage } from 'element-plus'; 
+
+const errorInterceptor = (error) => {
+    const errorMessage = error.response?.data?.message || 'An error occurred';
+    ElMessage.error(errorMessage); // Display error message using Element Plus
+    return Promise.reject(error);    
+};
+
+export default errorInterceptor;
--- src/core/services/orderService.ts
+++ src/core/services/orderService.ts
@@ -2,4 +2,4 @@ import baseService from "@/core/services/baseService";
 
 const url = import.meta.env.VITE_APP_API_URL;
 
-export default (version = 'v1') => baseService({ url, version });
+export default (version = 'v1', useInterceptor: boolean = true) => baseService({ url, version, useInterceptor });
